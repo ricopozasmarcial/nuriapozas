74a1cab8de52686d612c3a9469178850da4b1c33
Gema Rico Pozas <36774778+gemarico@users.noreply.github.com>
2020-09-15 16:21:41 +0200

    Updates

--- precache-manifest.bb292c8125aff955ed9cf1158aee5c77.js
+++ precache-manifest.bb292c8125aff955ed9cf1158aee5c77.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "0ce4ef9b2858e3383662",
+    "revision": "8c9750cd124846f96335",
     "url": "/nuriapozas/static/css/main.392610bc.chunk.css"
   },
   {
-    "revision": "0ce4ef9b2858e3383662",
-    "url": "/nuriapozas/static/js/main.0ce4ef9b.chunk.js"
+    "revision": "8c9750cd124846f96335",
+    "url": "/nuriapozas/static/js/main.8c9750cd.chunk.js"
   },
   {
     "revision": "6f09bd82d6f23d1d736e",
@@ -20,27 +20,27 @@ self.__precacheManifest = [
     "url": "/nuriapozas/static/js/runtime~main.0585fe4a.js"
   },
   {
-    "revision": "674f50d287a8c48dc19ba404d20fe713",
-    "url": "/nuriapozas/static/media/fontawesome-webfont.674f50d2.eot"
-  },
-  {
     "revision": "af7ae505a9eed503f8b8e6982036873e",
     "url": "/nuriapozas/static/media/fontawesome-webfont.af7ae505.woff2"
   },
   {
-    "revision": "b06871f281fee6b241d60582ae9369b9",
-    "url": "/nuriapozas/static/media/fontawesome-webfont.b06871f2.ttf"
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/nuriapozas/static/media/fontawesome-webfont.674f50d2.eot"
   },
   {
     "revision": "fee66e712a8a08eef5805a46892932ad",
     "url": "/nuriapozas/static/media/fontawesome-webfont.fee66e71.woff"
   },
   {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/nuriapozas/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
     "revision": "912ec66d7572ff821749319396470bde",
     "url": "/nuriapozas/static/media/fontawesome-webfont.912ec66d.svg"
   },
   {
-    "revision": "3401cecfceb06ddb9fc3e325ec6773c5",
+    "revision": "f66fe16a0f073fbf08fecb655175d8e0",
     "url": "/nuriapozas/index.html"
   }
 ];
