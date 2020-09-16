80065e0763c016b0f519c257160ac1753e9ce6f6
Gema Rico Pozas <36774778+gemarico@users.noreply.github.com>
2020-09-16 21:35:50 +0200

    Updates

--- precache-manifest.4b3f9dd8f0be9c8ad53ae8d4f60f7e6e.js
+++ precache-manifest.4b3f9dd8f0be9c8ad53ae8d4f60f7e6e.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "5b8966947822726b6a06",
-    "url": "/nuriapozas/static/css/main.5514622d.chunk.css"
+    "revision": "a71986d039c53c9bd3a0",
+    "url": "/nuriapozas/static/css/main.7ec93345.chunk.css"
   },
   {
-    "revision": "5b8966947822726b6a06",
-    "url": "/nuriapozas/static/js/main.5b896694.chunk.js"
+    "revision": "a71986d039c53c9bd3a0",
+    "url": "/nuriapozas/static/js/main.a71986d0.chunk.js"
   },
   {
     "revision": "9d99c80ed89489da9a68",
@@ -20,10 +20,6 @@ self.__precacheManifest = [
     "url": "/nuriapozas/static/js/runtime~main.0585fe4a.js"
   },
   {
-    "revision": "af7ae505a9eed503f8b8e6982036873e",
-    "url": "/nuriapozas/static/media/fontawesome-webfont.af7ae505.woff2"
-  },
-  {
     "revision": "674f50d287a8c48dc19ba404d20fe713",
     "url": "/nuriapozas/static/media/fontawesome-webfont.674f50d2.eot"
   },
@@ -36,11 +32,15 @@ self.__precacheManifest = [
     "url": "/nuriapozas/static/media/fontawesome-webfont.fee66e71.woff"
   },
   {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/nuriapozas/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
     "revision": "912ec66d7572ff821749319396470bde",
     "url": "/nuriapozas/static/media/fontawesome-webfont.912ec66d.svg"
   },
   {
-    "revision": "683f618465ae64d130ad534b802116cc",
+    "revision": "0515886f86c137f337dd22f66f4dafcc",
     "url": "/nuriapozas/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nuriapozas/precache-manifest.605a61c6bbbe15d89893f66daaa32125.js"
+  "/nuriapozas/precache-manifest.4b3f9dd8f0be9c8ad53ae8d4f60f7e6e.js"
 );
 
 workbox.clientsClaim();
