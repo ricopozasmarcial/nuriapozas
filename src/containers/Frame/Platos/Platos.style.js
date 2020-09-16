5caca85ee49553f637dc3856879e4d262ec9b052
Gema Rico Pozas <36774778+gemarico@users.noreply.github.com>
2020-09-16 20:40:31 +0200

    Update Platos.style.js

--- src/containers/Frame/Platos/Platos.style.js
+++ src/containers/Frame/Platos/Platos.style.js
@@ -1,18 +1,18 @@
 import styled from "styled-components";
 
 export const DivStyle = styled.div`
-    position: relative;
-    display: block;
-    background-color: black;
-    background-position: 50% 50%;
-    background-size: cover;
-    border-radius: 50%;
-    width: 60%;
-    height: auto;
-    padding-top: 60%;
-    margin: auto;
-    margin-bottom: 2%;
-    object-fit: cover;
-    z-index: 1;
-    overflow: hidden;
+	position: relative;
+	display: block;
+	background-image: url("img/fondoPlato.png");
+	background-position: 50% 50%;
+	background-size: 120%;
+	border-radius: 50%;
+	width: 60%;
+	height: 60%;
+	padding-top: 60%;
+	margin: auto;
+	margin-bottom: 2%;
+	object-fit: cover;
+	z-index: 1;
+	overflow: hidden;
 `;
