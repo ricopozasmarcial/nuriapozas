4312292f02d1b90504bf136ac4349941f2d7c0a0
Gema Rico Pozas <36774778+gemarico@users.noreply.github.com>
2020-09-17 00:06:30 +0200

    Updates

--- precache-manifest.6a925de4e4807642b2702a00cc24b6e4.js
+++ precache-manifest.6a925de4e4807642b2702a00cc24b6e4.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "9ae8161a6b965d485b51",
-    "url": "/nuriapozas/static/css/main.d296c70f.chunk.css"
+    "revision": "744f4962eead0e50ae33",
+    "url": "/nuriapozas/static/css/main.f0562fd7.chunk.css"
   },
   {
-    "revision": "9ae8161a6b965d485b51",
-    "url": "/nuriapozas/static/js/main.9ae8161a.chunk.js"
+    "revision": "744f4962eead0e50ae33",
+    "url": "/nuriapozas/static/js/main.744f4962.chunk.js"
   },
   {
     "revision": "9d99c80ed89489da9a68",
@@ -20,27 +20,27 @@ self.__precacheManifest = [
     "url": "/nuriapozas/static/js/runtime~main.0585fe4a.js"
   },
   {
-    "revision": "674f50d287a8c48dc19ba404d20fe713",
-    "url": "/nuriapozas/static/media/fontawesome-webfont.674f50d2.eot"
-  },
-  {
     "revision": "af7ae505a9eed503f8b8e6982036873e",
     "url": "/nuriapozas/static/media/fontawesome-webfont.af7ae505.woff2"
   },
   {
-    "revision": "fee66e712a8a08eef5805a46892932ad",
-    "url": "/nuriapozas/static/media/fontawesome-webfont.fee66e71.woff"
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/nuriapozas/static/media/fontawesome-webfont.674f50d2.eot"
   },
   {
     "revision": "b06871f281fee6b241d60582ae9369b9",
     "url": "/nuriapozas/static/media/fontawesome-webfont.b06871f2.ttf"
   },
   {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/nuriapozas/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
     "revision": "912ec66d7572ff821749319396470bde",
     "url": "/nuriapozas/static/media/fontawesome-webfont.912ec66d.svg"
   },
   {
-    "revision": "5121996fda863b7d5b454073a6aed559",
+    "revision": "265e95e3d47cbc9f448a658dc1029040",
     "url": "/nuriapozas/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nuriapozas/precache-manifest.16c94ccd6fd2f106d99c5e243bfcaec0.js"
+  "/nuriapozas/precache-manifest.6a925de4e4807642b2702a00cc24b6e4.js"
 );
 
 workbox.clientsClaim();
