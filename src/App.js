15c2c274cf188de6785a03641c8f7b1d7f5a281f
Gema Rico Pozas <36774778+gemarico@users.noreply.github.com>
2020-10-02 22:04:20 +0200

    anuncio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
 import "./App.css";
+import Alert from "react-bootstrap/Alert";
+
 import NavBar from "./containers/NavBar/NavBar";
 import Options from "./containers/Frame/Options/Options";
 import Footer from "./containers/Footer/Footer";
 function App() {
 	return (
 		<div className="App">
+			<Alert
+				style={{
+					maxHeight: "45px",
+					padding: "none",
+					margin: "0",
+					fontFamily: "Montserrat, sans-serif",
+					backgroundColor: "#f2f2f2",
+					border: "none"
+				}}
+				variant="secondary"
+			>
+				Gastos de envío gratis a partir de 100€
+			</Alert>
 			<NavBar />
 			<Options />
 			<Footer />
